24328839e2946ba7db721cce3e8979b9f08efcb9
ponponutube-byte <ponpon.utube@gmail.com>
2025-12-13 01:13:34 +0900

    基本的なディレクトリ構成を作成 (#10)
    * 基本的なディレクトリ構成を作成
    
    * 鬼のgitkeep

--- src/main/main.js
+++ src/main/main.js
@@ -13,13 +13,13 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, "../preload/preload.js"),
       contextIsolation: true,
       nodeIntegration: false,
     },
   });
 
-  mainWindow.loadFile(path.join(__dirname, "index.html"));
+  mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
 
   // 開発者ツールを開く
   mainWindow.webContents.openDevTools();
